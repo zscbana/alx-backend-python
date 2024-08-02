eb0773fa6d42c1186156f9acc2b89ea546e97ddf
zscbana <zscbana@gmail.com>
2024-08-02 05:25:42 +0300

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Task 5's module.'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Calculates the sum of all elements in a list.'''
+    return float(sum(input_list))
