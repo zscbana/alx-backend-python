c040f89be14783d6e462f934770febe21c79bfa7
zscbana <zscbana@gmail.com>
2024-08-02 05:17:53 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Task 2's module.'''
+import math
+
+
+def floor(n: float) -> float:
+    '''Return the largest integer less than or equal to n.'''
+    return math.floor(n)
