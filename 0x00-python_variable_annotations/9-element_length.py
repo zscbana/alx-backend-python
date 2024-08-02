20b384142d89f90523725395dc2aedf348e6950c
zscbana <zscbana@gmail.com>
2024-08-02 05:41:15 +0300

    code style for task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,5 +4,6 @@ from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    '''Returns a list of tuples where each tuple contains a sequence from the input'''
+    '''Returns a list of tuples where each tuple
+      contains a sequence from the input'''
     return [(i, len(i)) for i in lst]
