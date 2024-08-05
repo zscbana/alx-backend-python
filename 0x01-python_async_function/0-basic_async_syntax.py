eff4cd893c957a7eede4202d1c5481d6b3e422a3
zscbana <zscbana@gmail.com>
2024-08-05 22:00:43 +0300

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" synchronous coroutine that takes in an integer argument """
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ asynchronous coroutine that takes in an integer argument """
+    delay = random.random() * max_delay
+    await asyncio.sleep(delay)
+    return delay
