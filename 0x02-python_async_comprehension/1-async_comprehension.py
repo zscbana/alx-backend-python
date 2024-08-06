b21c7579c9ccbd291657e48944eb9e3ca9c77899
zscbana <zscbana@gmail.com>
2024-08-06 13:47:14 +0300

    Task 1 and 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,6 +8,6 @@ import typing
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension() -> float:
+async def async_comprehension() -> List[float]:
     """Async comprehension"""
-    pass
+    return [num async for num in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -3,4 +3,15 @@
 import asyncio
 import random
 import typing
+import time
 
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+async def measure_runtime() -> float:
+    """Measure the runtime of four parallel comprehensions"""
+    Start_time= time.time()
+    task = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*task)
+    end_time = time.time()
+    return (Start_time - end_time)
