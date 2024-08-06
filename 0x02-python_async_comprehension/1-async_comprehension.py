65d719b7eafbd497584ad54dc51bca1df31728d6
zscbana <zscbana@gmail.com>
2024-08-06 12:16:55 +0300

    fix readme

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""1. Async Comprehensions"""
+import asyncio
+import random
+import typing
+
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> float:
+    """Async comprehension"""
+    pass
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""2. Run time for four parallel comprehensions"""
+import asyncio
+import random
+import typing
+
