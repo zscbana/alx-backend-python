a484b02bee9f9279ce3c037fe4acff7010474262
zscbana <zscbana@gmail.com>
2024-08-07 10:16:12 +0300

    task 2 correction

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """2. Run time for four parallel comprehensions"""
 import asyncio
-import random
-import typing
 import time
 
 
@@ -14,5 +12,4 @@ async def measure_runtime() -> float:
     Start_time = time.time()
     task = [async_comprehension() for i in range(4)]
     await asyncio.gather(*task)
-    end_time = time.time()
-    return (Start_time - end_time)
+    return time.time() - Start_time
